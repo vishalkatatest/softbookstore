0124b8a4f7f31dcc3f4d8c5cbb75cea6b6fb39a1
Vishal <vishalsolankee@yahoo.co.in>
2026-05-27 12:23:06 +0530

    Refactored: Discount percentage rules configured in properties file

---
+++ src/main/java/com/softkata/softbookstore/domain/DiscountProperties.java
@@ -0,0 +1,15 @@
+package com.softkata.softbookstore.domain;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "books")
+@Data
+public class DiscountProperties {
+    private int discountFor2Books;
+    private int discountFor3Books;
+    private int discountFor4Books;
+    private int discountFor5Books;
+}
--- src/main/java/com/softkata/softbookstore/services/DiscountCalculatorService.java
+++ src/main/java/com/softkata/softbookstore/services/DiscountCalculatorService.java
@@ -1,17 +1,24 @@
 package com.softkata.softbookstore.services;
 
+import com.softkata.softbookstore.domain.DiscountProperties;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DiscountCalculatorService {
 
+    DiscountProperties discountProperties;
+
+    public DiscountCalculatorService(DiscountProperties discountProperties) {
+        this.discountProperties = discountProperties;
+    }
+
     public int getDiscount(int totalBooks) {
 
         return  switch (totalBooks) {
-            case 2 -> 5;
-            case 3 -> 10;
-            case 4 -> 20;
-            case 5 -> 25;
+            case 2 -> discountProperties.getDiscountFor2Books();
+            case 3 -> discountProperties.getDiscountFor3Books();
+            case 4 -> discountProperties.getDiscountFor4Books();
+            case 5 -> discountProperties.getDiscountFor5Books();
             default -> 0;
         };
 
