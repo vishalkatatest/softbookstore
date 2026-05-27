8a63eff5d88d5b5af5eae429833672a25bf7e902
Vishal <vishalsolankee@yahoo.co.in>
2026-05-27 11:52:31 +0530

    Initial Project Setup: created maven project with Springboot dependencies

---
+++ src/main/java/com/softkata/softbookstore/SoftbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.softkata.softbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftbookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftbookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softkata/softbookstore/SoftbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softkata.softbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftbookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
