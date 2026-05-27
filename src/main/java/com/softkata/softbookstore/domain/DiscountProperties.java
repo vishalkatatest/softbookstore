127b9a716458da0da2068900099dd43b8a81b636
Vishal <vishalsolankee@yahoo.co.in>
2026-05-27 12:37:31 +0530

    Refactored: Updated Discount percentage rules labels configured in properties file

--- src/main/java/com/softkata/softbookstore/domain/DiscountProperties.java
+++ src/main/java/com/softkata/softbookstore/domain/DiscountProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "book-discount")
+@ConfigurationProperties(prefix = "app.book-discount")
 @Data
 public class DiscountProperties {
     private int discountFor2Books;
